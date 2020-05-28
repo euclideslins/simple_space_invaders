104c24a43e78f4950f0bde225dee5d2b3c206180
Euclides LIns <euclides@pop-os.localdomain>
2020-05-28 15:15:45 -0400

    Shooting mechanic started

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ pygame.init()
 
 ##CREATE SCREEN
 screen = pygame.display.set_mode((800,600))
-
+background = pygame.image.load("assets/background.jpg")
 pygame.display.set_caption('Space Invaders')
+
 
 enemyImg = pygame.image.load("assets/alien.png")
 enemyX = random.randint(0,800)
@@ -15,17 +16,32 @@ enemyX_change=4
 enemyY_change=40
 
 
-background = pygame.image.load("assets/background.jpg")
 playerImg = pygame.image.load("assets/player64.png")
 playerX = 370
 playerY = 480
 playerX_change=0
+
+#Bullet
+
+# 1- ready - you cannot see the bullet
+# 2- fire - the bullet is on move
+
+bulletImg = pygame.image.load("assets/bullet.png")
+bulletX = 0
+bulletY = 480
+bulletY_change=10
+bullet_state = "ready"
 
 
 def player(x,y):
     screen.blit(playerImg, (x,y))
 def enemy(x,y):
     screen.blit(enemyImg, (x,y))
+def bullet(x,y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x+16, y+10))
+
 
 ##Loop game
 running = True
@@ -41,6 +57,8 @@ while running:
                 playerX_change = -3
             if(event.key == pygame.K_RIGHT):
                 playerX_change = 3
+            if(event.key == pygame.K_SPACE):
+                bullet(playerX, bulletY)
         if(event.type == pygame.KEYUP):
             if(event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT ):
                 playerX_change = 0
@@ -64,6 +82,10 @@ while running:
         enemyX_change = -4
         enemyY += enemyY_change
 
+
+    if bullet_state is "fire":
+        bullet(playerX, bulletY)
+        bulletY -= bulletY_change
 
     player(playerX,playerY)
     enemy(enemyX,enemyY)
